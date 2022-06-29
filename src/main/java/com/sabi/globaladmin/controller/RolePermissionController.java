143b67f02227fbecec6c9e2fb6e42f232ff9a972
ekedon <ekene.a@spinel.consulting>
2022-06-29 12:06:54 +0100

    new

--- src/main/java/com/sabi/globaladmin/controller/RolePermissionController.java
+++ src/main/java/com/sabi/globaladmin/controller/RolePermissionController.java
@@ -16,6 +16,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping(Constants.APP_CONTENT+"rolepermission")
@@ -105,5 +107,21 @@ public class RolePermissionController {
         httpCode = HttpStatus.OK;
         return new ResponseEntity<>(resp, httpCode);
     }
+
+
+
+
+    @DeleteMapping("")
+    public ResponseEntity<Response> removeMultiplePermission(@RequestParam("ids") List<Long> ids){
+        HttpStatus httpCode ;
+        Response resp = new Response();
+        service.deleteAllBYIds(ids);
+        resp.setCode(CustomResponseCode.SUCCESS);
+        resp.setDescription("Permission removed successfully !");
+        httpCode = HttpStatus.OK;
+        return new ResponseEntity<>(resp, httpCode);
+    }
+
+
 
 }
--- src/main/java/com/sabi/globaladmin/repository/RolePermissionRepository.java
+++ src/main/java/com/sabi/globaladmin/repository/RolePermissionRepository.java
@@ -6,10 +6,12 @@ import com.sabi.globaladmin.model.RolePermission;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 
@@ -28,5 +30,11 @@ public interface RolePermissionRepository extends JpaRepository<RolePermission, 
 
     @Query("SELECT p FROM RolePermission p WHERE ((:roleId IS NULL) OR (:roleId IS NOT NULL AND p.roleId = :roleId)) order by p.id")
     List<RolePermission> getPermissionsByRole(@Param("roleId") Long roleId);
+
+
+    @Modifying
+    @Transactional
+    @Query("delete from RolePermission p where p.id in ?1")
+    void deleteByIdIn(List<Long> ids);
 
 }
